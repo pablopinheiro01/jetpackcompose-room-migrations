8775f2cfc395f7a6421a852e9bd4397c149256b6
pablopinheiro01 <pablo-pinheiro@hotmail.com>
2023-11-16 20:13:26 -0300

    implementado consumo dos usuarios no dialog para troca de usuario com um clique

--- app/src/main/java/br/com/alura/helloapp/ui/userDialog/ListaUsuariosViewModel.kt
+++ app/src/main/java/br/com/alura/helloapp/ui/userDialog/ListaUsuariosViewModel.kt
@@ -2,21 +2,26 @@ package br.com.alura.helloapp.ui.userDialog
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.alura.helloapp.database.UsuarioDao
+import br.com.alura.helloapp.preferences.PreferencesKey
 import br.com.alura.helloapp.util.ID_USUARIO_ATUAL
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ListaUsuariosViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val dataStore: DataStore<Preferences>
+    private val dataStore: DataStore<Preferences>,
+    private val usuarioDao: UsuarioDao
 ) : ViewModel() {
 
     private val usuarioAtual = savedStateHandle.get<String>(ID_USUARIO_ATUAL)
@@ -32,5 +37,28 @@ class ListaUsuariosViewModel @Inject constructor(
     }
 
     private suspend fun carregaDados() {
+        usuarioAtual?.let {
+            val usuarioLogado = usuarioDao.buscaPorId(it).first()
+            usuarioLogado?.let {
+                _uiState.value = _uiState.value.copy(
+                    nomeDeUsuario = usuarioLogado.idUsuario,
+                    nome = usuarioLogado.nome
+                )
+            }
+        }
+
+        usuarioDao.buscaTodos().collect{ outrasContas ->
+            _uiState.value = _uiState.value.copy(
+                outrasContas = outrasContas.filter { it.idUsuario != usuarioAtual }
+            )
+        }
     }
+
+    suspend fun atualizaUsuarioLogado(novoUsuario: String) {
+        dataStore.edit {
+            it[PreferencesKey.USUARIO_ATUAL] = novoUsuario
+        }
+    }
+
+
 }
